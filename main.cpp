c7179a60e8ffde14a9242fa356cd647cb175de3a
Thiago de Barros Lacerda <tbl2@cin.ufpe.br>
2014-09-18 10:48:51 -0300

    Dump the used parameters in the application
    Signed-off-by: Thiago de Barros Lacerda <tbl2@cin.ufpe.br>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 #include "config.h"
 #include "manager.h"
 
+void dumpParameters()
+{
+    std::cout << "******** Parameters ********" << std::endl;
+    std::cout << "* Number of trajectories per flock: " << Config::numberOfTrajectoriesPerFlock() << std::endl;
+    std::cout << "* Length of flocks (seconds): " << Config::flockLength() << std::endl;
+    std::cout << "* Grid size (meters): " << Config::gridSize() << std::endl;
+    std::cout << "* Time slot size (seconds): " << Config::timeSlotSize() << std::endl;
+    std::cout << "* Used Coordinate System: " << Config::coordinateSystemName(Config::coordinateSystem()) << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     if (argc < 7) {
@@ -21,6 +31,7 @@ int main(int argc, char** argv)
     Config::setTimeSlotSize(atof(argv[4]));
     Config::setDecoder(argv[5]);
     Config::setCoordinateSystem(Config::CoordinateSystem(atoi(argv[6])));
+    dumpParameters();
     std::vector<std::string> decoderParams;
     for (int i = 7; i < argc; ++i)
         decoderParams.push_back(std::string(argv[i]));
