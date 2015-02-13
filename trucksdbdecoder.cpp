814fc59d15a781715513f490e82e3cd6401ee06f
Thiago de Barros Lacerda <tbl2@cin.ufpe.br>
2015-02-12 23:25:03 -0800

    Fix TrucksDBDecoder
    Signed-off-by: Thiago de Barros Lacerda <tbl2@cin.ufpe.br>

--- trucksdbdecoder.cpp
+++ trucksdbdecoder.cpp
@@ -23,12 +23,12 @@ void TrucksDBDecoder::doDecodeRow(void* row)
     tId = m_manager->getColumnAsUnsignedLong(row, 0);
     dateStr = m_manager->getColumnAsString(row, 2);
     hourStr = m_manager->getColumnAsString(row, 3);
-    latitude = m_manager->getColumnAsDouble(row, 4);
-    longitude = m_manager->getColumnAsDouble(row, 5);
+    longitude = m_manager->getColumnAsDouble(row, 4);
+    latitude = m_manager->getColumnAsDouble(row, 5);
 
     // tm.tm_year = year - 1900
     tm.tm_year = atoi(dateStr.substr(6, 4).c_str()) - 1900;
-    tm.tm_mon = atoi(dateStr.substr(4, 2).c_str());
+    tm.tm_mon = atoi(dateStr.substr(4, 2).c_str()) - 1;
     tm.tm_mday = atoi(dateStr.substr(0, 2).c_str());
     tm.tm_hour = atoi(hourStr.substr(0, 2).c_str());
     tm.tm_min = atoi(hourStr.substr(3, 2).c_str());
