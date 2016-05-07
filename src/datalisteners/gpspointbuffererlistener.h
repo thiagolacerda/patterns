f82582d63d324cf7c2d727dd7e41e5fd26d68690
Thiago Lacerda <tbl2@cin.ufpe.br>
2016-05-06 21:53:24 -0700

    Add concrete implementation of GPSPointBuffererListener
    Signed-off-by: Thiago Lacerda <tbl2@cin.ufpe.br>

---
+++ src/datalisteners/gpspointbuffererlistener.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include "datalistener.h"
+
+class GPSPoint;
+
+class GPSPointBuffererListener : public DataListener {
+public:
+    GPSPointBuffererListener(const std::unordered_map<std::string, std::string>& parameters);
+
+    void onDataReceived(const DataModel& dataModel) override;
+
+    void onDataProviderFinished() override;
+
+private:
+    void addPresence(uint32_t tID, uint64_t n);
+    void complete();
+    void insertInMap(const std::shared_ptr<GPSPoint>& dataModel, uint64_t timeSlot);
+    bool isOutlier(uint64_t timestamp, uint32_t trajectoryId, double latMeters, double longMeters);
+    void shiftPresenceMap();
+
+    uint32_t m_timeSlotSize;
+    uint32_t m_patternLength;
+    double m_outlierSpeedCutoff;
+    bool m_interpolate;
+    uint64_t m_lastTimeSlotSeen;
+    std::map<uint64_t, std::unordered_map<uint32_t, std::vector<std::shared_ptr<GPSPoint>>>> m_pointsPerTimeSlot;
+    std::unordered_map<uint32_t, uint64_t> m_pointPresence;
+    std::unordered_map<uint32_t, uint64_t> m_lastTimestampPerTrajectory;
+};
+
---
+++ src/datalisteners/gpspointbuffererlistenerdata.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include "processordata.h"
+
+class GPSPoint;
+
+class GPSPointBuffererListenerData : public ProcessorData {
+public:
+    GPSPointBuffererListenerData(const std::unordered_map<uint32_t, std::vector<std::shared_ptr<GPSPoint>>>& points,
+            const std::unordered_map<uint32_t, uint64_t>& presenceMap, uint32_t size, uint64_t timestamp)
+        : m_points(points)
+        , m_presenceMap(presenceMap)
+        , m_extent(size)
+        , m_timestamp(timestamp)
+    {
+    }
+
+    const std::unordered_map<uint32_t, std::vector<std::shared_ptr<GPSPoint>>>& m_points;
+    const std::unordered_map<uint32_t, uint64_t>& m_presenceMap;
+    uint32_t m_extent;
+    uint64_t m_timestamp;
+};
+
